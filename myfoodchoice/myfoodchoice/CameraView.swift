7c52bded3f38471367578db4c70da8f82653729f
serena599 <138121744+serena599@users.noreply.github.com>
2025-01-08 17:26:59 +1100

    Add files via upload

---
+++ myfoodchoice/myfoodchoice/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  RecipeSearch
+//
+//  Created by chris on 9/12/2024.
+//
+
+// reserved for camera function, need real iPhone to test
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text("CameraView")
+    }
+}
+
+#Preview {
+    CameraView()
+}
