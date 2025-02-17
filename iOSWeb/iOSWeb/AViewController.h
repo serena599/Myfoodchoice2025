3c544f6334bd42b4da1bb86968f249c8bee44715
serena599 <138121744+serena599@users.noreply.github.com>
2025-02-17 12:07:15 +0800

    Delete iOSWeb directory

--- iOSWeb/iOSWeb/AViewController.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface AViewController : UIViewController
-
-@end 
--- iOSWeb/iOSWeb/WebViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
-
-@interface WebViewController : UIViewController <WKNavigationDelegate, WKScriptMessageHandler>
-
-- (instancetype)initWithURL:(NSURL *)url;
-
-@end
